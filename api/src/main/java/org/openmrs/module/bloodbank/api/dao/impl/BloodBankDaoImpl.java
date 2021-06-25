0af8da9868ac2e299c4f18e7e041a4d346c26e83
nedhro <nedhro786>
2021-06-25 16:10:41 +0600

    Physical suitability, blood stock tracing mapping with blood donor

--- api/src/main/java/org/openmrs/module/bloodbank/api/dao/impl/BloodBankDaoImpl.java
+++ api/src/main/java/org/openmrs/module/bloodbank/api/dao/impl/BloodBankDaoImpl.java
@@ -18,114 +18,110 @@ import java.util.List;
 
 public class BloodBankDaoImpl implements BloodBankDao {
 
-    protected final Logger log = LoggerFactory.getLogger(BloodDonorDaoImpl.class);
+	protected final Logger log = LoggerFactory.getLogger(BloodDonorDaoImpl.class);
 
-    private SessionFactory sessionFactory;
+	private SessionFactory sessionFactory;
 
-    private Session getSession() {
-        return sessionFactory.getCurrentSession();
-    }
+	private Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
 
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
-    @Override
-    public List<BloodDonorPhysicalSuitability> getAllDonorTestsResult() {
-        Criteria criteria = getSession().createCriteria(BloodDonorPhysicalSuitability.class);
-        criteria.add(Restrictions.eq("donorSelection", PermissionType.Selected));
-        return criteria.list();
-    }
+	@Override
+	public List<BloodDonorPhysicalSuitability> getAllDonorTestsResult() {
+		Criteria criteria = getSession().createCriteria(BloodDonorPhysicalSuitability.class);
+		criteria.add(Restrictions.eq("donorSelection", PermissionType.Selected));
+		return criteria.list();
+	}
 
-    @Override
-    @Transactional
-    public BloodCompatibility saveBloodCompatibility(BloodCompatibility bloodCompatibility) {
-        getSession().persist(bloodCompatibility);
-        log.info("Blood compatibility has been saved successfully..." + bloodCompatibility);
-        return bloodCompatibility;
-    }
+	@Override
+	@Transactional
+	public BloodCompatibility saveBloodCompatibility(BloodCompatibility bloodCompatibility) {
+		getSession().persist(bloodCompatibility);
+		log.info("Blood compatibility has been saved successfully..." + bloodCompatibility);
+		return bloodCompatibility;
+	}
 
-    @Override
-    @Transactional
-    public BloodCompatibility updateBloodCompatibility(BloodCompatibility bloodCompatibility) {
-        getSession().update(bloodCompatibility);
-        log.info("Blood compatibility has been updated successfully..." + bloodCompatibility);
-        return bloodCompatibility;
-    }
+	@Override
+	@Transactional
+	public BloodCompatibility updateBloodCompatibility(BloodCompatibility bloodCompatibility) {
+		getSession().update(bloodCompatibility);
+		log.info("Blood compatibility has been updated successfully..." + bloodCompatibility);
+		return bloodCompatibility;
+	}
 
-    @Override
-    public List<BloodCompatibility> getAllBloodCompatibility() {
-        Criteria criteria = getSession().createCriteria(BloodCompatibility.class);
-        criteria.add(Restrictions.eq("status", 1));
-        log.info("Blood Compatibility List ::" + criteria.list());
-        return criteria.list();
-    }
+	@Override
+	public List<BloodCompatibility> getAllBloodCompatibility() {
+		Criteria criteria = getSession().createCriteria(BloodCompatibility.class);
+		criteria.add(Restrictions.eq("status", 1));
+		log.info("Blood Compatibility List ::" + criteria.list());
+		return criteria.list();
+	}
 
-    @Override
-    @Transactional
-    public BloodCompatibility getBloodCompatibilityById(Integer id) {
-        Criteria criteria = getSession().createCriteria(BloodCompatibility.class);
-        criteria.add(Restrictions.eq("bloodCompatibilityId", id));
-        BloodCompatibility compatibility = (BloodCompatibility) criteria.uniqueResult();
-        return compatibility;
-    }
+	@Override
+	@Transactional
+	public BloodCompatibility getBloodCompatibilityById(Integer id) {
+		Criteria criteria = getSession().createCriteria(BloodCompatibility.class);
+		criteria.add(Restrictions.eq("bloodCompatibilityId", id));
+		BloodCompatibility compatibility = (BloodCompatibility) criteria.uniqueResult();
+		return compatibility;
+	}
 
-    @Override
-    public BloodStockTracing saveBloodStockTracing(BloodStockTracing bloodStockTracing) {
-        if (uniqueBloodBagId(bloodStockTracing.getBloodBagId())) {
-            getSession().persist(bloodStockTracing);
-            log.info("Blood Stock Tracing info has been saved successfully..." + bloodStockTracing);
-            return bloodStockTracing;
-        } else {
-            return null;
-        }
-    }
+	@Override
+	public BloodStockTracing saveBloodStockTracing(BloodStockTracing bloodStockTracing) {
+		if (uniqueBloodBagId(bloodStockTracing.getBloodBagId())) {
+			getSession().persist(bloodStockTracing);
+			log.info("Blood Stock Tracing info has been saved successfully..." + bloodStockTracing);
+			return bloodStockTracing;
+		} else {
+			return null;
+		}
+	}
 
-    @Override
-    public BloodStockTracing updateBloodStockTracing(BloodStockTracing bloodStockTracing) {
-        getSession().update(bloodStockTracing);
-        log.info("Blood Stock Tracing info has been updated successfully..." + bloodStockTracing);
-        return bloodStockTracing;
+	@Override
+	public BloodStockTracing updateBloodStockTracing(BloodStockTracing bloodStockTracing) {
+		getSession().update(bloodStockTracing);
+		log.info("Blood Stock Tracing info has been updated successfully..." + bloodStockTracing);
+		return bloodStockTracing;
 
-    }
+	}
 
-    @Override
-    public List<BloodStockTracing> getAllBloodStockTracing() {
-        Criteria criteria = getSession().createCriteria(BloodStockTracing.class);
-        criteria.add(Restrictions.eq("status", 1));
-        criteria.add(Restrictions.eq("stockStatus", StockStatus.Available));
-        log.info("Blood Stock Tracing List ::" + criteria.list());
-        return criteria.list();
-    }
+	@Override
+	public List<BloodStockTracing> getAllBloodStockTracing() {
+		Criteria criteria = getSession().createCriteria(BloodStockTracing.class);
+		criteria.add(Restrictions.eq("status", 1));
+		criteria.add(Restrictions.eq("stockStatus", StockStatus.Available));
+		log.info("Blood Stock Tracing List ::" + criteria.list());
+		return criteria.list();
+	}
 
-    @Override
-    public BloodStockTracing getBloodStockTracingById(Integer id) {
-        Criteria criteria = getSession().createCriteria(BloodStockTracing.class);
-        criteria.add(Restrictions.eq("bloodStockTracingId", id));
-        BloodStockTracing bloodStockTracing = (BloodStockTracing) criteria.uniqueResult();
-        return bloodStockTracing;
-    }
+	@Override
+	public BloodStockTracing getBloodStockTracingById(Integer id) {
+		Criteria criteria = getSession().createCriteria(BloodStockTracing.class);
+		criteria.add(Restrictions.eq("bloodStockTracingId", id));
+		BloodStockTracing bloodStockTracing = (BloodStockTracing) criteria.uniqueResult();
+		return bloodStockTracing;
+	}
 
-    @Override
-    public BloodStockTracing getBloodStockTracingByBloodBagId(String bloodBagId) {
-        Criteria criteria = getSession().createCriteria(BloodStockTracing.class);
-        criteria.add(Restrictions.eq("bloodBagId", bloodBagId));
-        criteria.add(Restrictions.eq("status", 1));
-        criteria.add(Restrictions.eq("stockStatus", StockStatus.Available));
-        BloodStockTracing bloodStockTracing = (BloodStockTracing) criteria.uniqueResult();
-        return bloodStockTracing;
-    }
+	@Override
+	public BloodStockTracing getBloodStockTracingByBloodBagId(String bloodBagId) {
+		Criteria criteria = getSession().createCriteria(BloodStockTracing.class);
+		criteria.add(Restrictions.eq("bloodBagId", bloodBagId));
+		criteria.add(Restrictions.eq("status", 1));
+		criteria.add(Restrictions.eq("stockStatus", StockStatus.Available));
+		BloodStockTracing bloodStockTracing = (BloodStockTracing) criteria.uniqueResult();
+		return bloodStockTracing;
+	}
 
-    public Boolean uniqueBloodBagId(String bloodBagId) {
-        Criteria criteria = getSession().createCriteria(BloodStockTracing.class);
-        criteria.add(Restrictions.eq("bloodBagId", bloodBagId));
-        criteria.add(Restrictions.eq("status", 1));
-        criteria.add(Restrictions.eq("stockStatus", StockStatus.Available));
-        BloodStockTracing bloodStockTracing = (BloodStockTracing) criteria.uniqueResult();
-        if (bloodStockTracing == null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public Boolean uniqueBloodBagId(String bloodBagId) {
+		Criteria criteria = getSession().createCriteria(BloodStockTracing.class);
+		criteria.add(Restrictions.eq("bloodBagId", bloodBagId));
+		criteria.add(Restrictions.eq("status", 1));
+		criteria.add(Restrictions.eq("stockStatus", StockStatus.Available));
+		BloodStockTracing bloodStockTracing = (BloodStockTracing) criteria.uniqueResult();
+		return bloodStockTracing == null;
+	}
 }
